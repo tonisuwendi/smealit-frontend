2dce10c7432782e0fd6a2fe36006b62795cc7040
Toni Suwendi <tonisuwendi2002@gmail.com>
2020-02-20 15:46:07 +0700

    first commit

---
+++ src/container/Auth/Login.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Component } from "react";
+import "./Auth.css";
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="form">
+          <h2 className="title">Login Akun!</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
